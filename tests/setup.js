29a59ec9d3de662a1b7336e6b7ed97df3e6a60cd
s1vann <140653998+sivvv0@users.noreply.github.com>
2026-06-25 23:31:32 +0300

    Create setup.js

---
+++ tests/setup.js
@@ -0,0 +1,12 @@
+// Silence console logs during tests
+global.console = {
+  ...console,
+  log: jest.fn(),
+  debug: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+};
+
+// Increase timeout for async tests
+jest.setTimeout(30000);
